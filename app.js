4c6b2d78871d51678ff8982a356e2c132b287bd0
JohndylJaculba <baldoscielomarie2@gmail.com>
2026-04-26 19:54:52 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+function postAnnouncement() {
+  // get input value
+  let message = document.getElementById("inputBox").value;
+
+  // display to output
+  document.getElementById("outputBox").innerText = message;
+}
